100d15265f49a032f3c1107c3fd810eb19892f60
BK <3031011@naver.com>
2024-05-17 18:37:33 +0900

    홈 버튼 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,7 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>its app layout</header>
+        <header>
+          <Link href="/">홈으로</Link>
+        </header>
+
         {children}
       </body>
     </html>
