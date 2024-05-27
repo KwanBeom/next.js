5165c8e8e32a225f19e2bb8c92623c3c04cdb91f
BK <3031011@naver.com>
2024-05-27 19:12:26 +0900

    soft navigation

--- src/app/parallelroutes/basic/page.tsx
+++ src/app/parallelroutes/basic/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   // soft navigation으로 settings를 탐색하면 에러가 발생하지 않는다.
   return (
     <>
-      <Link href="/parallelroutes/settings">
+      <Link href="/parallelroutes/basic/settings">
         soft navigation으로 settings 탐색
       </Link>
     </>
